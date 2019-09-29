ae4feeb6edb6d152df1d8d0233fdf99c79067e93
anwar1938anwar <55034228+anwar1938anwar@users.noreply.github.com>
2019-09-29 06:54:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,7 +27,7 @@ client.on("message", message => {
 
   client.on("message", message => {
   
-              if (message.content.startsWith(prefix + "bc")) {
+              if (message.content.startsWith(prefix + "/bc")) {
                            if (!message.member.hasPermission("ADMINISTRATOR"))  return;
     let args = message.content.split(" ").slice(1);
     var argresult = args.join(' '); 
