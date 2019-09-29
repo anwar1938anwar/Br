55d8282a8c5f8dce11bf7e758d296dce91b2689a
anwar1938anwar <55034228+anwar1938anwar@users.noreply.github.com>
2019-09-29 06:59:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,17 +6,21 @@ var adminprefix = '/'
 
 //bc
 
-client.on("message", message => {
-    if (message.content.startsWith("/obc")) {
-                 if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' ');
-  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
-  m.send(`${argresult}\n ${m}`);
+
+  var prefix = "/";
+
+  client.on("message", message => {
+  
+              if (message.content.startsWith(prefix + "bc")) {
+                           if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+    let args = message.content.split(" ").slice(1);
+    var argresult = args.join(' '); 
+    message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+   m.send(`${argresult}\n ${m}`);
   })
-  message.channel.send(`\`${message.guild.members.filter( m => m.presence.status !== 'all').size}\`:mailbox:  عدد المستلمين `);
-  message.delete();
-  };
+   message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'all').size}\` :mailbox:  عدد المستلمين `); 
+   message.delete(); 
+  };     
   });
 
 
